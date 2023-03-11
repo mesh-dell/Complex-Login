d3a4ee5baad9185fc135f417c1b9c57fad0fb007
mesh-dell <mwaimonicas@gmail.com>
2023-03-11 14:40:09 +0300

    Added firebase functionality

--- app/src/main/java/com/example/logindemo/Register.java
+++ app/src/main/java/com/example/logindemo/Register.java
@@ -1,5 +1,7 @@
 package com.example.logindemo;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Build;
@@ -8,8 +10,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Register extends AppCompatActivity {
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://console.firebase.google.com/project/complex-login-659ae/database/complex-login-659ae-default-rtdb/data/~2F");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +44,54 @@ public class Register extends AppCompatActivity {
                 final String passwordTxt = password.getText().toString();
                 final String conpasswordTxt = conpassword.getText().toString();
 
+                if(fullnameTxt.isEmpty() || emailTxt.isEmpty() || phoneTxt.isEmpty() || passwordTxt.isEmpty())
+                {
+                    Toast.makeText(Register.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+                }
+                //checking passwords
+                else if (!passwordTxt.equals(conpasswordTxt)) {
+                    Toast.makeText(Register.this, "Passwords are not matching", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    databaseReference.child("users").addChildEventListener(new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                            if (snapshot.hasChild(phoneTxt))
+                            {
+                                Toast.makeText(Register.this, "Phone is already registered", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                //adding data to firebase
+                                databaseReference.child("users").child(phoneTxt).child("fullname").setValue(fullnameTxt);
+                                databaseReference.child("users").child(phoneTxt).child("email").setValue(emailTxt);
+                                databaseReference.child("users").child(phoneTxt).child("password").setValue(passwordTxt);
+
+                                Toast.makeText(Register.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
             }
         });
         loginNowBtn.setOnClickListener(new View.OnClickListener() {
