a64f1eaa0169903a6ffd4300f258212dc1c32aa8
mesh-dell <mwaimonicas@gmail.com>
2023-03-11 14:17:18 +0300

    UI update

--- app/src/main/java/com/example/logindemo/Register.java
+++ app/src/main/java/com/example/logindemo/Register.java
@@ -2,7 +2,12 @@ package com.example.logindemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        final EditText fullname = findViewById(R.id.fullname);
+        final EditText email = findViewById(R.id.email);
+        final  EditText phone = findViewById(R.id.phone);
+        final EditText password = findViewById(R.id.password);
+        final EditText conpassword = findViewById(R.id.conpassword);
+
+        final Button registerBtn = findViewById(R.id.registerBtn);
+        final TextView loginNowBtn = findViewById(R.id.loginNow);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String fullnameTxt = fullname.getText().toString();
+                final String emailTxt = email.getText().toString();
+                final String phoneTxt = phone.getText().toString();
+                final String passwordTxt = password.getText().toString();
+                final String conpasswordTxt = conpassword.getText().toString();
+
+            }
+        });
+        loginNowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/logindemo/login.java
+++ app/src/main/java/com/example/logindemo/login.java
@@ -2,6 +2,7 @@ package com.example.logindemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,6 +34,13 @@ public class login extends AppCompatActivity {
                 else{
 
                 }
+            }
+        });
+        registerNowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(login.this, Register.class));
+
             }
         });
     }
